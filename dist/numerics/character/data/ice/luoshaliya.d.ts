4f53465951220f23400b3e7cf6778120b2a589be
wormtql <584130248@qq.com>
2021-04-07 18:38:35 +0800

    feat(chatacters): add 罗莎莉亚(Rosaria)

---
+++ dist/numerics/character/data/ice/luoshaliya.d.ts
@@ -0,0 +1,3 @@
+import CharacterDataType from "../../data_type";
+declare const _default: CharacterDataType;
+export default _default;
---
+++ src/numerics/character/data/ice/luoshaliya.ts
@@ -0,0 +1,14 @@
+import WeaponType from "../../../weapon/weapon_type";
+import CharacterDataType from "../../data_type";
+
+export default {
+    alt: ["luoshaliya", "罗莎莉亚", "rosaria"],
+    life: [1030, 2647, 3417, 5118, 5665, 6516, 7245, 8096, 8643, 9493, 10040, 10891, 11438, 12289],
+    attack: [20, 52, 67, 100, 111, 127, 141, 158, 169, 185, 196, 213, 223, 240],
+    defend: [60, 153, 197, 296, 327, 376, 418, 468, 499, 548, 580, 629, 661, 710    ],
+    secondary: {
+        name: "attackPercentage",
+        family: "atk240",
+    },
+    weapon: WeaponType.Stick,
+} as CharacterDataType
