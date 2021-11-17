1743642a40a2695ec0d8f03d0c4722b671af8ba0
wormtql <584130248@qq.com>
2021-11-17 13:07:48 +0800

    feat: Add traveller-electro

---
+++ src/numerics/character/data/thunder/me_electro.ts
@@ -0,0 +1,16 @@
+import { Element } from "../../../../common/element";
+import WeaponType from "../../../weapon/weapon_type";
+import CharacterDataType from "../../data_type";
+
+export default {
+    element: Element.Thunder,
+    alt: ["me_thunder", "旅行者-雷", "me_electro"],
+    life: [912, 2342, 3024, 4529, 5013, 5766, 6411, 7164, 7648, 8401, 8885, 9638, 10122, 10875],
+    attack: [18, 46, 59, 88, 98, 113, 125, 140, 149, 164, 174, 188, 198, 212],
+    defend: [57, 147, 190, 284, 315, 362, 402, 450, 480, 527, 558, 605, 635, 683],
+    secondary: {
+        name: "attackPercentage",
+        family: "atk240"
+    },
+    weapon: WeaponType.Sword,
+} as CharacterDataType
